20f9c3f879915ec80f45799b2193789b5eb6abe4
B Karthik <bangikarthik7@gmail.com>
2025-02-18 19:41:39 +0530

    firebase setup

--- App.tsx
+++ App.tsx
@@ -1,13 +1,35 @@
 import { ScreenContent } from './src/components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
+import { db } from './src/firebase/config';
+import { collection, getDocs } from 'firebase/firestore';
 
 import './global.css';
+import { useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [data, setData] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(collection(db, 'testing'));
+      const docsData = querySnapshot.docs.map((doc) => doc.data());
+      setData(docsData);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <>
-      <ScreenContent title="Home" path="App.tsx" />
-      <StatusBar style="auto" />
-    </>
+    <View>
+      <Text>Data from Firestore:</Text>
+      {data.map((item, index) => (
+        <View key={index}>
+          <Text>{JSON.stringify(item, null, 2)}</Text>
+        </View>
+      ))}
+    </View>
   );
-}
+};
+
+export default App;
